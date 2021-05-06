0df6df8d60e887f3c2567d561dd333545bd49e51
Andrew <447187604@qq.com>
2021-05-07 00:27:30 +0800

    排除不需要的启动依赖

--- docdocker-consumer-666/src/main/java/com/gauge/docdocker/Consumer_666_StartSpringCloudApplication.java
+++ docdocker-consumer-666/src/main/java/com/gauge/docdocker/Consumer_666_StartSpringCloudApplication.java
@@ -3,10 +3,13 @@ package com.gauge.docdocker;
 import com.gauge.commons.config.SmartLoadBalanceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
-@SpringBootApplication
+// 没有使用DataSource，则需要把spring-boot-starter-jdbc的依赖排除掉
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class })
 //@EnableEurekaClient
 //@RibbonClient(name="ribbonClient",configuration= SmartLoadBalanceConfig.class)
 public class Consumer_666_StartSpringCloudApplication {

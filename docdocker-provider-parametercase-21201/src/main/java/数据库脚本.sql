8a0d1ce034c574024e1bc33ca897238d048c447b
Andrew-GaoGuanWei <447187604@qq.com>
2021-06-07 23:51:27 +0800

    添加字段 interface_parameter

--- docdocker-provider-parametercase-21201/src/main/java/数据库脚本.sql
+++ docdocker-provider-parametercase-21201/src/main/java/数据库脚本.sql
@@ -17,3 +17,10 @@ INSERT INTO cool_api(api_name,api_desc) VALUES ('modifybyempid',database()) ;
 
 
 SELECT api_id,api_name,api_desc FROM cool_api
+
+-- 添加interface_parameter表中字段。 2021-06-07 21:54
+alter table interface_parameter add column value text COMMENT '请求参数JSON';
+alter table interface_parameter add column dev_operate_by varchar(36);
+alter table interface_parameter add column test_operate_by varchar(36);
+alter table interface_parameter add column operate_dt datetime(6) default CURRENT_TIMESTAMP(6);
+alter table interface_parameter add column last_update_dt datetime(6) default CURRENT_TIMESTAMP(6) on UPDATE CURRENT_TIMESTAMP(6);
